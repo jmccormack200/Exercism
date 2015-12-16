060ac8cddf2bc8074508aeceb5c965ffab14548c
John McCormack <mccormack.wgsi@gmail.com>
2015-12-16 17:32:03 -0500

    Had an idea for exerism but not enough time to test it out

--- erlang/bob/bob.erl
+++ erlang/bob/bob.erl
@@ -4,12 +4,16 @@
 response_for("") ->
 	"Fine. Be that way!";
 
+
+
 response_for(Question) ->
 	case [lists:last(Question)] == "?" of
 		true -> "Sure.";
 		false -> "Whatever."
 	end.
 
-
-
-
+%response_for(Questions) ->
+%	case string:to_upper(Questions) == Questions 
+%% Basically we want to check to see if the normal question is equal
+%% to the question if we uppercase it, basically meaning it was
+%% already uppercased.
